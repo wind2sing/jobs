6fbedc384908c69a9cc4c5ffa80ab99b29e578a1
wind2sing <wind2sing@gmail.com>
2023-02-03 20:27:30 +0800

    feat: jichang_checkin

---
+++ jichang_checkin.js
@@ -0,0 +1,27 @@
+const { gotScraping } = require("got-scraping");
+const { CookieJar } = require("tough-cookie");
+const { sendNotify } = require("./sendNotify");
+const url = process.env.JC_URL;
+const email = process.env.JC_EMAIL;
+const passwd = process.env.JC_PASSWD;
+const noti = process.env.JC_NOTI;
+const cookieJar = new CookieJar();
+const log = async (content) => {
+  console.log(content);
+  if (noti) await sendNotify(`机场签到 ${url}`, content);
+};
+async function main() {
+  log("正在登录...");
+  await gotScraping
+    .post(new URL("/auth/login", url), {
+      json: { email, passwd },
+      responseType: "json",
+      cookieJar,
+    })
+    .then((r) => log(r.body["msg"]));
+  await gotScraping
+    .post(new URL("/user/checkin", url), { responseType: "json", cookieJar })
+    .then((r) => log(r.body["msg"]));
+}
+
+main().catch((err) => log(err));
